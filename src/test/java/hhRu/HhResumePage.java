8180fa41fcae3cd7bd25038907d4471d8c574c23
Nickolay Petrych <npetrych@alfabank.ru>
2022-05-09 20:05:25 +0500

    add HashMap

--- src/test/java/hhRu/HhResumePage.java
+++ src/test/java/hhRu/HhResumePage.java
@@ -3,7 +3,11 @@ package hhRu;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.codeborne.selenide.Selenide.$x;
+import static javax.swing.UIManager.put;
 
 public class HhResumePage {
 
@@ -11,6 +15,16 @@ public class HhResumePage {
     private final SelenideElement age = $x("//span[@data-qa='resume-personal-age']/span");
     private final SelenideElement city = $x("//span[@data-qa='resume-personal-address']");
     private final SelenideElement liveData = $x("//span[@data-qa='resume-personal-address']/ancestor::p");
+    private final SelenideElement tick = $x("//div[@data-qa='resume-contacts-phone']/span[1]");
+
+    /**
+     * ключи для хешкарты
+     */
+    public static String GENDER = "Пол";
+    public static String CITY = "Город";
+    public static String AGE = "Возраст";
+    public static String Confirmed_Phone = "Подтвержденный номер";
+    public static String READY_TO_RELOCATE = "Готовность к переезду";
 
     /**
      * в конструкторе передаем url который находится в HhTest.java
@@ -18,6 +32,28 @@ public class HhResumePage {
      */
     public HhResumePage(String url) {
         Selenide.open(url);
+    }
+
+    /**
+     * метод проверяет наличие галочки на дисплее
+     * @return
+     */
+    public boolean isPhoneConfirmed() {
+       return tick.isDisplayed();
+    }
+
+    /**
+     * создали хешкарту и положили актуальные результаты
+     * @return
+     */
+    public Map<String,Object> getAttributes() {
+        return new HashMap<String, Object>(){{
+        put(GENDER, getGenderHard());
+        put(AGE, getAge());
+        put(CITY, getCity());
+        put(Confirmed_Phone, isPhoneConfirmed());
+        put(READY_TO_RELOCATE, isReadyToRelocate());
+            }};
     }
 
     public String getCity() {
